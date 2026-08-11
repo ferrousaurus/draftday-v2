a972fabdcb3c3d32b2434014bc9f0a7b0fb061b6
OxidalWave <github@oxidalwave.com>
2026-08-11 16:14:17 -0500

    Add varlock to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { varlockVitePlugin } from "@varlock/vite-integration";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [varlockVitePlugin()],
   resolve: {
     tsconfigPaths: true,
   },

f34b57a25b239ff5935436349c827a659d627b64
OxidalWave <github@oxidalwave.com>
2026-08-11 15:47:12 -0500

    Move vitest config to own file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tanstackStart({ spa: { enabled: true } }), nitro({ preset: "deno-deploy" }), viteReact()],
   resolve: {
     tsconfigPaths: true,
   },
-  test: {},
 });

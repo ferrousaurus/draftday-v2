22e89bf53ec38b47d7dd8cf5bba23ea7970e7d13
OxidalWave <github@oxidalwave.com>
2026-08-13 20:10:42 -0500

    Revert to nitro3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro-nightly/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
